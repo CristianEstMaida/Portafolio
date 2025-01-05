6277933079117ed5edfbd7d4101c2b459ca9e893
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2025-01-04 22:12:10 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,134 @@
+$(function() {
+
+    $('.navbar-toggle').click(function() {
+        $(this).toggleClass('act');
+            if($(this).hasClass('act')) {
+                $('.main-menu').addClass('act');
+            }
+            else {
+                $('.main-menu').removeClass('act');
+            }
+    });
+
+    $(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.icon-up-bg').slideDown(300);
+		} else {
+			$('.icon-up-bg').slideUp(300);
+		}
+	});
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $(document).on('click', '.page-scroll2 a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+	/* Progress bar */
+    var $section = $('.section-skills');
+    function loadDaBars() {
+	    $('.progress .progress-bar').progressbar({
+	        transition_delay: 500
+	    });
+    }
+    
+    $(document).bind('scroll', function(ev) {
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+            loadDaBars();
+            // unbind event not to load scrolsl again
+            $(document).unbind('scroll');
+        }
+    });
+
+    /* Counters  */
+    if ($(".section-counters .start").length>0) {
+        $(".section-counters .start").each(function() {
+            var stat_item = $(this),
+            offset = stat_item.offset().top;
+            $(window).scroll(function() {
+                if($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+	// another custom callback for counting to infinity
+	$('#infinity').data('countToOptions', {
+		onComplete: function (value) {
+		  count.call(this, {
+		    from: value,
+		    to: value + 1
+		  });
+		}
+	});
+
+	$('#infinity').each(count);
+
+	function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+    // Navigation overlay
+    var s = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: false,
+            mobileDeceleration: 0.004,
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+    });
+    
+    $("#contact-form").submit(function(e) {
+        e.preventDefault();
+    
+        // Get form data
+        const name = $("#name").val();
+        const email = $("#email").val();
+        const message = $("#message").val();
+    
+        // Create mailto link with form data
+        const mailtoLink = `mailto:cristian.esteban.maida.dev@gmail.com?subject=Contacto&body=Nombre: ${name}%0ACorreo electrónico: ${email}%0AMensaje: ${message}`;
+    
+        // Open mailto link
+        window.location.href = mailtoLink;
+      });
+
+    
+    $('#copiar-email').click(function() {
+        const email = "cristian.esteban.maida.dev@gmail.com";
+        navigator.clipboard.writeText(email)
+            .then(() => {
+                $('.alert').show();
+            })
+            .catch(err => {
+                console.error('Error al copiar:', err);
+            });
+    });
+
+    
+
+});
